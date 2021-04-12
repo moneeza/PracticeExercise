02a209eb045c10cc594cc8b85862a8294fd77f4e
moneeza <moneeza@3gca.org>
2021-04-12 13:04:22 +0500

    more tweak

--- src/common/CommonLayout/Sidebar.tsx
+++ src/common/CommonLayout/Sidebar.tsx
@@ -1,24 +1,28 @@
-import { Link} from 'react-router-dom'
-import { Layout, Menu} from 'antd';
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Layout, Menu } from 'antd';
 
 export default function SideBar() {
     const { Sider } = Layout;
-
-    return (<Sider>
+    const [coll, setColl] = useState(false)
+    const handleCllapse = () => {
+        coll ? setColl(false) : setColl(true)
+    }
+    return (<Sider collapsible collapsed={coll} onCollapse={handleCllapse}>
         <div className="d-flex flex-column align-items-center justify-content-center bg-transparent" style={{ marginTop: "5rem" }}>
-                <Menu theme="dark" mode="vertical" defaultSelectedKeys={['1']} className="w-100 text-center">
-                    <Menu.Item key="1">
-                        <Link to="/comp1">
-                            <div>Login</div>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Link to="/comp2">
+            <Menu theme="dark" mode="vertical" defaultSelectedKeys={['1']} className="w-100 text-center">
+                <Menu.Item key="1">
+                    <Link to="/comp1">
+                        <div>Login</div>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                    <Link to="/comp2">
 
-                            <div> Results</div>
-                        </Link>
-                    </Menu.Item>
-                </Menu>
+                        <div> Results</div>
+                    </Link>
+                </Menu.Item>
+            </Menu>
         </div>
     </Sider>)
 
